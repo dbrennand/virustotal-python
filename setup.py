034a5e2e258713c4db76615836154206a2e7873f
Dextroz <25749668+Dextroz@users.noreply.github.com>
2019-06-23 18:12:20 +0100

    Fix Unicode error when packing with Windows.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-with open("README.md", "r") as readme:
+with open("README.md", "r", encoding="utf8") as readme:
     long_description = readme.read()
 
 setup(
