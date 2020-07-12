5446bddd2184f04a3b0ac2f2fe5d1c033430af40
dbrennand <52419383+dbrennand@users.noreply.github.com>
2020-07-12 16:19:46 +0100

    Transfer ownership.

--- virustotal_python/virustotal.py
+++ virustotal_python/virustotal.py
@@ -1,7 +1,7 @@
 """
 MIT License
 
-Copyright (c) 2020 Dextroz (Dextroz)
+Copyright (c) 2020 dbrennand (dbrennand)
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
