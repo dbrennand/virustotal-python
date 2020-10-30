db7d612ca3133dfc95c5e2d57b8ef70395caf6c9
dbrennand <52419383+dbrennand@users.noreply.github.com>
2020-10-30 20:38:39 +0000

    Fix license in file. Remove ImportError print. Add API_VERSION parameter and docstring to __init__ Adjust headers for each API version. Raise exception when API_VERSION is invalid. file_scan: Update docstring, added new parameter upload_url, added support for v3 API. Add new method file_upload_url.

--- virustotal_python/virustotal.py
+++ virustotal_python/virustotal.py
@@ -1,7 +1,7 @@
 """
 MIT License
 
-Copyright (c) 2020 dbrennand (dbrennand)
+Copyright (c) 2020 dbrennand
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -19,57 +19,135 @@ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
 AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE."""
-try:
-    from requests import get, post
-    from os.path import abspath, basename
-except ImportError as err:
-    print(f"Failed to import required modules: {err}")
+SOFTWARE.
+"""
+from requests import get, post
+from os.path import abspath, basename
 
 
 class Virustotal(object):
     """
-    Base class for interacting with the Virustotal Public API. (https://www.virustotal.com/en/documentation/public-api/)
+    Base class for interacting with the public VirusTotal API [v2](https://www.virustotal.com/en/documentation/public-api/) and [v3](https://developers.virustotal.com/v3.0/reference).
     """
 
-    def __init__(self, API_KEY: str = None, PROXIES: dict = None):
+    def __init__(
+        self, API_KEY: str = None, PROXIES: dict = None, API_VERSION: str = "v2"
+    ):
+        """
+        Initalisation method for Virustotal class.
+
+        :param API_KEY: The API used to interact with the VirusTotal v2 and v3 APIs.
+        :param PROXIES: A dictionary object containing proxies used when making requests.
+        :param API_VERSION: The version to use when interacting with the VirusTotal API. This parameter defaults to 'v2' for backwards compatibility.
+        :raises ValueError: Raises ValueError when no API_KEY is provided or the API_VERSION is invalid.
+        """
+        self.VERSION = "0.1.0"
         self.API_KEY = API_KEY
-        self.PROXIES = PROXIES
-        self.BASEURL = "https://www.virustotal.com/vtapi/v2/"
-        self.VERSION = "0.0.9"
-        self.headers = {
-            "Accept-Encoding": "gzip, deflate",
-            "User-Agent": f"gzip,  virustotal-python {self.VERSION}",
-        }
         if API_KEY is None:
             raise ValueError(
                 "An API_KEY is required to interact with the VirusTotal API."
             )
+        self.PROXIES = PROXIES
+        # Declare appropriate variables depending on the API_VERSION provided
+        if API_VERSION == "v2":
+            self.API_VERSION = API_VERSION
+            self.BASEURL = "https://www.virustotal.com/vtapi/v2/"
+            self.HEADERS = {
+                "Accept-Encoding": "gzip, deflate",
+                "User-Agent": f"gzip,  virustotal-python {self.VERSION}",
+            }
+        elif API_VERSION == "v3":
+            self.API_VERSION = API_VERSION
+            self.BASEURL = "https://www.virustotal.com/api/v3/"
+            self.HEADERS = {
+                "Accept-Encoding": "gzip, deflate",
+                "User-Agent": f"gzip, virustotal-python {self.VERSION}",
+                "x-apikey": f"{self.API_KEY}",
+            }
+        else:
+            raise ValueError(
+                f"The API version {API_VERSION} is not a valid VirusTotal API version.\nValid API versions are: 'v2' or 'v3'."
+            )
 
-    def file_scan(self, file):
+    def file_scan(self, file, upload_url: str = None):
         """
-        Send a file to Virustotal for analysis. (https://developers.virustotal.com/v2.0/reference#file-scan)
-           :param file: The path to the file to be sent to Virustotal for analysis.
-           :rtype: A dictionary containing the resp_code and JSON response.
+        Send a file to VirusTotal for analysis. Max file size is 32MB.
+
+        [v2 documentation](https://developers.virustotal.com/v2.0/reference#file-scan)
+
+        [v3 documentation](https://developers.virustotal.com/v3.0/reference#files-scan)
+
+        :param file: The path to the file to be sent to VirusTotal for analysis.
+        :param upload_url: The URL used to upload files larger than 32MB to VirusTotal for analysis. Obtained from the `file_upload_url` method.
+        :returns: A dictionary containing the resp_code and JSON response.
         """
-        params = {"apikey": self.API_KEY}
         files = {"file": (basename(file), open(abspath(file), "rb"))}
-        resp = self.make_request(
-            f"{self.BASEURL}file/scan", params=params, files=files, proxies=self.PROXIES
-        )
+        if self.API_VERSION == "v3":
+            # If upload_url is provided, override default v3 API endpoint
+            if upload_url:
+                endpoint = upload_url
+            else:
+                # Use standard v3 API endpoint
+                endpoint = f"{self.BASEURL}files"
+            resp = self.make_request(endpoint, files=files, proxies=self.PROXIES)
+        else:
+            # If upload_url is provided, override default v2 API endpoint
+            if upload_url:
+                endpoint = upload_url
+            else:
+                # Use standard v2 API endpoint
+                endpoint = f"{self.BASEURL}file/scan"
+            # v2 API request
+            params = {"apikey": self.API_KEY}
+            resp = self.make_request(
+                endpoint,
+                params=params,
+                files=files,
+                proxies=self.PROXIES,
+            )
+        return resp
+
+    def file_upload_url(self):
+        """
+        Get a URL for uploading files larger than 32MB to the VirusTotal API.
+
+        NOTE: For the v2 API, this endpoint requires additional privileges. For the v3 API, no additional privileges are required based on the documentation.
+
+        [v2 documentation](https://developers.virustotal.com/reference#file-scan-upload-url)
+
+        [v3 documentation](https://developers.virustotal.com/v3.0/reference#files-upload-url)
+
+        :returns: A dictionary containing the resp_code and JSON response.
+        """
+        if self.API_VERSION == "v3":
+            resp = self.make_request(
+                f"{self.BASE_URL}files/upload_url",
+                method="GET",
+                proxies=self.PROXIES,
+            )
+        else:
+            # v2 API request
+            params = {"apikey": self.API_KEY}
+            resp = self.make_request(
+                f"{self.BASE_URL}file/scan/upload_url",
+                method="GET",
+                proxies=self.PROXIES,
+            )
         return resp
 
     def file_rescan(self, *resource: list):
         """
-        Resend a file to Virustotal for analysis. (https://developers.virustotal.com/v2.0/reference#file-rescan)
+        Resend a file to VirusTotal for analysis. (https://developers.virustotal.com/v2.0/reference#file-rescan)
            :param *resource: A list of resource(s) of a specified file(s). Can be `md5/sha1/sha256 hashes`. Can be a combination of any of the three allowed hashes (MAX 25 items).
            :rtype: A dictionary containing the resp_code and JSON response.
         """
-        raise DeprecationWarning("VirusTotal removed this API endpoint from the public API.")
+        raise DeprecationWarning(
+            "VirusTotal removed this API endpoint from the public API."
+        )
 
     def file_report(self, *resource: list):
         """
-        Retrieve scan report(s) for a given file from Virustotal. (https://developers.virustotal.com/v2.0/reference#file-report)
+        Retrieve scan report(s) for a given file from VirusTotal. (https://developers.virustotal.com/v2.0/reference#file-report)
            :param *resource: A list of resource(s) of a specified file(s). Can be `md5/sha1/sha256 hashes` and/or combination of hashes and scan_ids (MAX 4 per standard request rate).
            :rtype: A dictionary containing the resp_code and JSON response.
         """
@@ -84,7 +162,7 @@ class Virustotal(object):
 
     def url_scan(self, *url: list):
         """
-        Send url(s) to Virustotal. (https://developers.virustotal.com/v2.0/reference#url-scan)
+        Send url(s) to VirusTotal. (https://developers.virustotal.com/v2.0/reference#url-scan)
            :param *url: A list of url(s) to be scanned. (MAX 4 per standard request rate).
            :rtype: A dictionary containing the resp_code and JSON response.
         """
@@ -155,15 +233,15 @@ class Virustotal(object):
     def make_request(self, endpoint: str, params: dict, method="POST", **kwargs):
         """
         Helper function to make the request to the specified endpoint.
-           :param endpoint: The specific Virustotal API endpoint.
+           :param endpoint: The specific VirusTotal API endpoint.
            :param method: The request method to use.
            :param params: The parameters to go along with the request.
            :rtype: A dictionary containing the resp_code and JSON response.
         """
         if method == "POST":
-            resp = post(endpoint, params=params, headers=self.headers, **kwargs)
+            resp = post(endpoint, params=params, headers=self.HEADERS, **kwargs)
         elif method == "GET":
-            resp = get(endpoint, params=params, headers=self.headers, **kwargs)
+            resp = get(endpoint, params=params, headers=self.HEADERS, **kwargs)
         else:
             raise ValueError("Invalid request method.")
         return self.validate_response(resp)
