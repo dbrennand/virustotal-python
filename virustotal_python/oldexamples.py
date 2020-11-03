6e0b2bb4b781aaa5818317aa36b61b6958bee43d
dbrennand <52419383+dbrennand@users.noreply.github.com>
2020-11-03 00:05:41 +0000

    Replace examples.py as oldexamples.py

--- virustotal_python/oldexamples.py
+++ virustotal_python/oldexamples.py
@@ -1,18 +1,26 @@
+"""
+The examples below are for prior to version 0.1.0 of virustotal-python.
+
+Examples for versions >=0.1.0 head to /examples directory.
+"""
 from virustotal import Virustotal
 from pprint import pprint
+import os.path
 
 # Normal Initialisation.
-vtotal = Virustotal("Insert API Key Here.")
+vtotal = Virustotal("Insert API key here.")
 
 # NEW as of version 0.0.5: Proxy support.
 # Example Usage: Using HTTP(S)
 vtotal = Virustotal(
-    "Insert API Key Here.",
-    {"http": "http://10.10.1.10:3128", "https": "http://10.10.1.10:1080"})
+    "Insert API key here.",
+    {"http": "http://10.10.1.10:3128", "https": "http://10.10.1.10:1080"},
+)
 # Or using SOCKS
 vtotal = Virustotal(
-    "Insert API Key Here.",
-    {"http": "socks5://user:pass@host:port", "https": "socks5://user:pass@host:port"})
+    "Insert API key here.",
+    {"http": "socks5://user:pass@host:port", "https": "socks5://user:pass@host:port"},
+)
 
 # NOTE: Check virustotal.py for docstrings containing full parameter descriptions.
 
@@ -22,17 +30,17 @@ resp = vtotal.file_scan("./tests.py")  # PATH to file for querying.
 # NOTE: This endpoint has been removed from the Public Virustotal API.
 # Resend a file to Virustotal for analysis.
 # A list containing the resource (SHA256) HASH of the file above.
-#resp = vtotal.file_rescan(
+# resp = vtotal.file_rescan(
 #    ["75efd85cf6f8a962fe016787a7f57206ea9263086ee496fc62e3fc56734d4b53"]
-#)
+# )
 ## A list containing md5/sha1/sha256 hashes. Can be a combination of any of the three allowed hashes (MAX 25 items).
 ## NOTE: The second hash here is flagged as malicious by multiple engines.
-#resp = vtotal.file_rescan(
+# resp = vtotal.file_rescan(
 #    [
 #        "75efd85cf6f8a962fe016787a7f57206ea9263086ee496fc62e3fc56734d4b53",
 #        "9f101483662fc071b7c10f81c64bb34491ca4a877191d464ff46fd94c7247115",
 #    ]
-#)
+# )
 
 # Retrieve scan report(s) for given file(s) from Virustotal.
 # A list containing the resource (SHA256) HASH of a known malicious file.
