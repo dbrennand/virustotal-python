8c83c803091676cb9cf6c62eeda5f0984e8c1cac
dbrennand <52419383+dbrennand@users.noreply.github.com>
2020-11-07 19:46:34 +0000

    Add domain_info example.

---
+++ examples/domain_info.py
@@ -0,0 +1,32 @@
+"""
+The examples in this file are for virustotal-python version >=0.1.0
+
+Retrieve information about a domain from the VirusTotal API.
+
+Documentation:
+
+    * v2 documentation - https://developers.virustotal.com/reference#domain-report
+
+    * v3 documentation - https://developers.virustotal.com/v3.0/reference#domain-info
+"""
+from virustotal_python import Virustotal
+from pprint import pprint
+
+API_KEY = "Insert API key here."
+
+domain = "virustotal.com"
+
+# v3 example
+vtotal = Virustotal(API_KEY=API_KEY, API_VERSION="v3")
+
+resp = vtotal.request(f"domains/{domain}")
+
+pprint(resp.data)
+
+# v2 example
+vtotal = Virustotal(API_KEY=API_KEY)
+
+resp = vtotal.request("domain/report", params={"domain": domain})
+
+print(resp.response_code)
+pprint(resp.json())
