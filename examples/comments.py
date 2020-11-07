34db6690c0870631f492c89ab0b2eefed47e3e21
dbrennand <52419383+dbrennand@users.noreply.github.com>
2020-11-07 20:06:54 +0000

    Add start of comment.py example.

---
+++ examples/comments.py
@@ -0,0 +1,19 @@
+"""
+The examples in this file are for virustotal-python version >=0.1.0
+
+Retrieve comments and interact with them using the VirusTotal API.
+
+Documentation:
+
+    * v2 documentation - https://developers.virustotal.com/reference#comments-get
+
+        * https://developers.virustotal.com/reference#comments-put
+
+    * v3 documentation - https://developers.virustotal.com/v3.0/reference#comments-1
+"""
+from virustotal_python import Virustotal
+from pprint import pprint
+
+API_KEY = "Insert API key here."
+
+# v3 example
