c03611b2a9fad872c860a8d7a5be27297ed9c5b2
dbrennand <52419383+dbrennand@users.noreply.github.com>
2020-11-20 18:49:58 +0000

    Edit example description. Remove repeated code.

--- examples/scan_file.py
+++ examples/scan_file.py
@@ -1,7 +1,7 @@
 """
 The examples in this file are for virustotal-python version >=0.1.0
 
-Send a file to VirusTotal for analysis.
+Send a file to the VirusTotal API for analysis.
 
 Documentation:
 
@@ -15,14 +15,14 @@ from pprint import pprint
 
 API_KEY = "Insert API key here."
 
-# v3 example
-vtotal = Virustotal(API_KEY=API_KEY, API_VERSION="v3")
-
 # Declare PATH to file
 FILE_PATH = "/path/to/file/to/scan.txt"
 
 # Create dictionary containing the file to send for multipart encoding upload
 files = {"file": (os.path.basename(FILE_PATH), open(os.path.abspath(FILE_PATH), "rb"))}
+
+# v3 example
+vtotal = Virustotal(API_KEY=API_KEY, API_VERSION="v3")
 
 resp = vtotal.request("files", files=files, method="POST")
 
@@ -30,12 +30,6 @@ pprint(resp.data)
 
 # v2 example
 vtotal = Virustotal(API_KEY=API_KEY)
-
-# Declare PATH to file
-FILE_PATH = "/path/to/file/to/scan.txt"
-
-# Create dictionary containing the file to send for multipart encoding upload
-files = {"file": (os.path.basename(FILE_PATH), open(os.path.abspath(FILE_PATH), "rb"))}
 
 resp = vtotal.request("file/scan", files=files, method="POST")
 
