7514d1dc147e6102e14eb875c0cd39e488703327
dbrennand <52419383+dbrennand@users.noreply.github.com>
2020-11-20 18:44:35 +0000

    Move comment down.

--- examples/file_info.py
+++ examples/file_info.py
@@ -12,12 +12,12 @@ Documentation:
 from virustotal_python import Virustotal
 from pprint import pprint
 
-# v3 example
 API_KEY = "Insert API key here."
 
 # The ID (either SHA-256, SHA-1 or MD5) identifying the file
 FILE_ID = "9f101483662fc071b7c10f81c64bb34491ca4a877191d464ff46fd94c7247115"
 
+# v3 example
 vtotal = Virustotal(API_KEY=API_KEY, API_VERSION="v3")
 
 resp = vtotal.request(f"files/{FILE_ID}")
