fb83ff86001d5c5926b8f45468dd1fd13921f030
dbrennand <52419383+dbrennand@users.noreply.github.com>
2020-11-20 18:41:33 +0000

    Alter wording.

--- examples/cursor.py
+++ examples/cursor.py
@@ -1,7 +1,7 @@
 """
 The examples in this file are for virustotal-python version >=0.1.0
 
-Use a cursor from the VirusTotal API response JSON to retrieve more results.
+Use a cursor from the VirusTotal API JSON response to retrieve more results.
 
 Documentation:
 
@@ -21,6 +21,7 @@ vtotal = Virustotal(API_KEY=API_KEY, API_VERSION="v3")
 # Retrieve communicating_files related to the IP address with a limit of 5
 resp = vtotal.request(f"ip_addresses/{IP}/communicating_files", params={"limit": 2})
 
+# Initialise count variable
 count = 0
 
 # While a cursor is present, keep collecting results!
@@ -30,5 +31,5 @@ while resp.cursor:
     # Get more results with cursor
     resp = vtotal.request(f"ip_addresses/{IP}/communicating_files", params={"limit": 2, "cursor": resp.cursor})
     # Do something with the resp here
-    # Add to count to show how many times we have got another cursor
+    # Add to the count to show how many times we have retrieved another cursor
     count += 1
