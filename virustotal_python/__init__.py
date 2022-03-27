75298c4920490bb7943b0bd079b7528c7ff677e5
dbrennand <52419383+dbrennand@users.noreply.github.com>
2022-03-27 13:56:25 +0100

    refactor: add docstring and module dunder names.

--- virustotal_python/__init__.py
+++ virustotal_python/__init__.py
@@ -1,3 +1,10 @@
+"""A Python library to interact with the public VirusTotal v2 and v3 APIs.
+"""
 from virustotal_python.virustotal import Virustotal
 from virustotal_python.virustotal import VirustotalError
+
 name = "virustotal-python"
+
+__all__ = ["Virustotal", "VirustotalError"]
+__author__ = "dbrennand"
+__version__ = "0.3.0"
