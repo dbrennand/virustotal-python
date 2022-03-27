c0d45ea40d298f75964a7182909461e7a6a6c567
dbrennand <52419383+dbrennand@users.noreply.github.com>
2022-03-27 18:03:13 +0100

    feat: push `pytest` and `pytest_mock` tests progress.

--- tests/test_virustotal_python.py
+++ tests/test_virustotal_python.py
@@ -1,102 +1,70 @@
 """Tests for virustotal-python.
 """
-import os
-import typing
 import virustotal_python
-import time
 import pytest
-
-# Declare variables for tests
-# Create dictionary containing the file to send for multipart encoding upload
-FILES = {
-    "file": (
-        os.path.basename("virustotal_python/oldexamples.py"),
-        open(os.path.abspath("virustotal_python/oldexamples.py"), "rb"),
-    )
-}
-
-# The ID (either SHA-256, SHA-1 or MD5) identifying the file
-FILE_ID = "9f101483662fc071b7c10f81c64bb34491ca4a877191d464ff46fd94c7247115"
-# Example IP address (Cloudflare DNS)
-IP = "1.1.1.1"
-# Example ID of a graph
-## NOTE: There are no comments on this graph so an empty list is returned
-GRAPH_ID = "g70fae134aefc4e2f90f069aba47d15a92e0073564310443aa0b6ca3384f5240d"
-# URL/domain identifier
-URL_DOMAIN = "github.com"
-# Example comment ID
-COMMENT_ID = (
-    "f-9f101483662fc071b7c10f81c64bb34491ca4a877191d464ff46fd94c7247115-07457619"
-)
-
-
-def check_resp_code(resp: typing.Union[virustotal_python.Virustotal, dict]) -> bool:
-    """Check the HTTP status code returned from the VirusTotal API is 200 (OK)."""
-    # Using COMPATIBILITY_ENABLED parameter
-    if isinstance(resp, dict):
-        assert resp["status_code"] == 200
-    else:
-        assert resp.status_code == 200
-
-
-@pytest.fixture(autouse=True)
-def avoid_rate_limit(request) -> None:
-    """Fixture to wait 15 seconds between API requests to avoid the VirusTotal API rate limit.
-    The Public API is limited to ... a rate of 4 requests per minute.
-
-    Args:
-        request: request-context object - https://docs.pytest.org/en/6.2.x/fixture.html#request-context
-    """
-
-    def fin():
-        """Finaliser function to wait 15 seconds."""
-        time.sleep(15)
-
-    request.addfinalizer(fin)
-
+import pytest_mock
 
 @pytest.fixture
-def vtotal(request) -> virustotal_python.Virustotal:
-    """Fixture to initialise the Virustotal class with provided parameters for a test.
+def mock_os_environ(mocker: pytest_mock.MockerFixture) -> None:
+    """Mocker fixture for `os.environ`.
 
     Args:
-        request: request-context object - https://docs.pytest.org/en/6.2.x/fixture.html#request-context
-
-    Returns:
-        virustotal_python.Virustotal: A virustotal_python.Virustotal object.
+        mocker (pytest_mock.MockerFixture): A pytest_mock.MockerFixture providing a
+            thin-wrapper around the patching API from the mock library.
     """
-    return virustotal_python.Virustotal(**request.param)
+    mocker.patch.dict("os.environ", dict(VIRUSTOTAL_API_KEY="API key"))
 
+def test_virustotal_apikey_env(mock_os_environ) -> None:
+    """Test `Virustotal` environment variable `VIRUSTOTAL_API_KEY`.
 
-class Test_Virustotal:
-    """A class containing tests for the Virustotal class."""
+    Args:
+        mock_os_environ: Pytest fixture function to mock `os.environ`.
+    """
+    vtotal = virustotal_python.Virustotal()
+    assert vtotal.API_KEY == "API key"
 
-    @pytest.mark.parametrize(
-        "vtotal",
-        [{"API_VERSION": 2, "COMPATIBILITY_ENABLED": True}],
-        indirect=True,
-    )
-    def test_v2_compatibility(self, vtotal: virustotal_python.Virustotal) -> None:
-        """Test Virustotal class `COMPATIBILITY_ENABLED` parameter behaviour with the v2 API.
+def test_virustotal(mocker: pytest_mock.MockerFixture) -> None:
+    """Test `Virustotal` parameters.
 
-        Args:
-            vtotal (virustotal_python.Virustotal): The Virustotal object returned from the `vtotal` fixture.
-        """
-        # Get domain report
-        resp = vtotal.request("domain/report", params={"domain": URL_DOMAIN})
-        check_resp_code(resp)
+    Args:
+        mocker (pytest_mock.MockerFixture): A pytest_mock.MockerFixture providing a
+            thin-wrapper around the patching API from the mock library.
+    """
+    keyword_arguments = {"API_KEY": "test", "API_VERSION": 3, "PROXIES": {"http": "http://10.10.1.10:3128"}, "TIMEOUT": 10}
+    mock_vtotal = mocker.patch("virustotal_python.Virustotal")
+    virustotal_python.Virustotal(**keyword_arguments)
+    mock_vtotal.assert_called_with(**keyword_arguments)
 
-    @pytest.mark.parametrize(
-        "vtotal",
-        [{"API_VERSION": 3, "COMPATIBILITY_ENABLED": True}],
-        indirect=True,
-    )
-    def test_v3_compatibility(self, vtotal: virustotal_python.Virustotal) -> None:
-        """Test Virustotal class `COMPATIBILITY_ENABLED` parameter behaviour with the v3 API.
+def test_virustotal_context_manager() -> None:
+    """Test `Virustotal` context manager support."""
+    with virustotal_python.Virustotal(API_KEY="test") as vtotal:
+        vtotal.__exit__("", "", "")
 
-        Args:
-            vtotal (virustotal_python.Virustotal): The Virustotal object returned from the `vtotal` fixture.
-        """
-        # Get domain report
-        resp = vtotal.request(f"domains/{URL_DOMAIN}")
-        check_resp_code(resp)
+def test_virustotal_api_key_value_error() -> None:
+    """Test `Virustotal` raises `ValueError` when no `API_KEY` is provided."""
+    with pytest.raises(ValueError) as execinfo:
+        virustotal_python.Virustotal()
+    assert "An API key is required to interact with the VirusTotal API.\nProvide one to the API_KEY parameter or by setting the environment variable 'VIRUSTOTAL_API_KEY'." == str(execinfo.value)
+
+def test_virustotal_api_version_value_error() -> None:
+    """Test `Virustotal` raises `ValueError` when an unsupported `API_VERSION` is provided."""
+    with pytest.raises(ValueError) as execinfo:
+        virustotal_python.Virustotal(API_KEY="test", API_VERSION="test")
+    assert "The API version 'test' is not a valid VirusTotal API version.\nValid API versions are 'v2', 2, 'v3' and 3." == str(execinfo.value)
+
+def test_request_notimplemented_error() -> None:
+    """Test `Virustotal.request()` raises `NotImplementedError` when an unsupported `method` is provided."""
+    with pytest.raises(NotImplementedError) as execinfo:
+        vtotal = virustotal_python.Virustotal(API_KEY="test")
+        vtotal.request("test", method="test")
+    assert "The request method 'test' is not implemented." == str(execinfo.value)
+
+"""
+# Tests
+
+Test request `large_file` parameter
+
+Test VirustotalResponse properties and methods
+
+Test VirustotalError properties and methods
+"""
