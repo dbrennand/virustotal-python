a03a794f1aaeb958036345e9b3ae4d6a074c444c
dbrennand <52419383+dbrennand@users.noreply.github.com>
2022-04-02 10:27:40 +0100

    refactor(example): update URL and formatting.

--- examples/graphs.py
+++ examples/graphs.py
@@ -5,25 +5,31 @@ Retrieve graphs and interact with them using the VirusTotal v3 API.
 
 Documentation:
 
-    * v3 documentation - https://developers.virustotal.com/v3.0/reference#graphs-1
+    * v3 documentation - https://developers.virustotal.com/reference/graphs
 """
 from virustotal_python import Virustotal
-from pprint import pprint
 
-API_KEY = "Insert API key here."
+API_KEY = "<VirusTotal API Key>"
 
 # Example ID of a graph
 GRAPH_ID = "g70fae134aefc4e2f90f069aba47d15a92e0073564310443aa0b6ca3384f5240d"
 
 # v3 examples
-vtotal = Virustotal(API_KEY=API_KEY, API_VERSION="v3")
+vtotal = Virustotal(API_KEY=API_KEY)
 
-## Retrieve 3 graphs from the VirusTotal v3 API
+# Get 3 graphs from the VirusTotal API
 resp = vtotal.request("graphs", params={"limit": 3})
-## Retrieve 3 graphs from the VirusTotal v3 API filtering by owner, order and attributes
-resp = vtotal.request("graphs", params={"limit": 2, "filter": "owner:hugoklugman", "order": "views_count", "attributes": "graph_data"})
-### Retrieve a graph using the graph's ID
+# Get 3 graphs from the VirusTotal API filtering by owner, order and attributes
+resp = vtotal.request(
+    "graphs",
+    params={
+        "limit": 2,
+        "filter": "owner:hugoklugman",
+        "order": "views_count",
+        "attributes": "graph_data",
+    },
+)
+# Get a graph using the graph's ID
 resp = vtotal.request(f"graphs/{GRAPH_ID}")
 
-# For more graph endpints, see https://developers.virustotal.com/v3.0/reference#graphs-1
 # To create a graph, head to https://www.virustotal.com/graph/
