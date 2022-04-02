da23954689d551ee0379aacb8d144d20c82ab909
dbrennand <52419383+dbrennand@users.noreply.github.com>
2022-04-02 10:55:15 +0100

    refactor(example): Update example urls and usage.

--- examples/searchmetadata.py
+++ examples/searchmetadata.py
@@ -7,33 +7,33 @@ Also, retrieve VirusTotal metadata.
 
 Documentation:
 
-    * v3 documentation - https://developers.virustotal.com/v3.0/reference#search-1
+    * v3 documentation
 
-        * https://developers.virustotal.com/v3.0/reference#metadata
+        https://developers.virustotal.com/reference/search-1
+        https://developers.virustotal.com/reference/metadata
 """
 from virustotal_python import Virustotal
-from pprint import pprint
 
-API_KEY = "Insert API key here."
+API_KEY = "<VirusTotal API Key>"
 
-# The ID (either SHA-256, SHA-1 or MD5) identifying the file
+# The ID (either SHA-256, SHA-1 or MD5 hash) identifying the file
 FILE_ID = "9f101483662fc071b7c10f81c64bb34491ca4a877191d464ff46fd94c7247115"
 
 # v3 examples
-vtotal = Virustotal(API_KEY=API_KEY, API_VERSION="v3")
+vtotal = Virustotal(API_KEY=API_KEY)
 
-## Search the VirusTotal API for google.com
+# Search the VirusTotal API for google.com
 resp = vtotal.request("search", params={"query": "google.com"})
-## Search the VirusTotal API for information related to Google's DNS (8.8.8.8)
+# Search the VirusTotal API for information related to Google's DNS (8.8.8.8)
 resp = vtotal.request("search", params={"query": "8.8.8.8"})
-## Search the VirusTotal API for a file ID
+# Search the VirusTotal API for a file ID
 resp = vtotal.request("search", params={"query": FILE_ID})
-## Search the VirusTotal API for the tag comment '#malicious'
+# Search the VirusTotal API for the tag comment '#malicious'
 resp = vtotal.request("search", params={"query": "#malicious"})
 
-## Retrieve VirusTotal metadata
+# Retrieve VirusTotal metadata
 resp = vtotal.request("metadata")
-## Print out a list of VirusTotal's supported engines
+# Print out a list of VirusTotal's supported engines
 resp = vtotal.request("metadata")
 engines_dict = resp.data["engines"]
 print(engines_dict.keys())
