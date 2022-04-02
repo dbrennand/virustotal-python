34b9b7cec3163df092d9e3f6a5f5d49fd9d6f303
dbrennand <52419383+dbrennand@users.noreply.github.com>
2022-04-02 10:39:37 +0100

    refactor(example): update documentation links and usage.

--- examples/scan_file.py
+++ examples/scan_file.py
@@ -5,42 +5,31 @@ Send a file to the VirusTotal API for analysis.
 
 Documentation:
 
-    * v2 documentation - https://developers.virustotal.com/reference#file-scan
+    * v3 documentation
 
-    * v3 documentation - https://developers.virustotal.com/v3.0/reference#files-scan
+        https://developers.virustotal.com/reference/files-scan
+        https://developers.virustotal.com/reference/files-upload-url
 
-        * https://developers.virustotal.com/reference/files-upload-url
+    * v2 documentation - https://developers.virustotal.com/v2.0/reference/file-scan
 """
 from virustotal_python import Virustotal
 import os.path
 from pprint import pprint
 
-API_KEY = "Insert API key here."
+API_KEY = "<VirusTotal API Key>"
 
-# Declare PATH to file
 FILE_PATH = "/path/to/file/to/scan.txt"
 
 # Create dictionary containing the file to send for multipart encoding upload
 files = {"file": (os.path.basename(FILE_PATH), open(os.path.abspath(FILE_PATH), "rb"))}
 
-# v2 example
-vtotal = Virustotal(API_KEY=API_KEY)
-
-resp = vtotal.request("file/scan", files=files, method="POST")
-
-print(resp.response_code)
-pprint(resp.json())
-
 # v3 example
-vtotal = Virustotal(API_KEY=API_KEY, API_VERSION="v3")
-
+vtotal = Virustotal(API_KEY=API_KEY)
 resp = vtotal.request("files", files=files, method="POST")
-
 pprint(resp.data)
 
 # v3 example for uploading a file larger than 32MB in size
-vtotal = Virustotal(API_KEY=API_KEY, API_VERSION="v3")
-
+vtotal = Virustotal(API_KEY=API_KEY)
 # Create dictionary containing the large file to send for multipart encoding upload
 large_file = {
     "file": (
@@ -50,6 +39,12 @@ large_file = {
 }
 # Get URL to send a large file
 upload_url = vtotal.request("files/upload_url").data
-# Submit large file to VirusTotal v3 API for analysis
+# Submit large file to VirusTotal for analysis
 resp = vtotal.request(upload_url, files=large_file, method="POST", large_file=True)
 pprint(resp.data)
+
+# v2 example
+vtotal = Virustotal(API_KEY=API_KEY, API_VERSION=2)
+resp = vtotal.request("file/scan", files=files, method="POST")
+print(resp.response_code)
+pprint(resp.json())
