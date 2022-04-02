47eec934170b4eec2c26143cf015a94a1e8741db
dbrennand <52419383+dbrennand@users.noreply.github.com>
2022-04-02 10:34:47 +0100

    refactor(example): update docs and general updates.

--- examples/ip.py
+++ examples/ip.py
@@ -5,44 +5,42 @@ Retrieve IP addresses using the VirusTotal API.
 
 Documentation:
 
-    * v2 documentation - https://developers.virustotal.com/reference#ip-address-report
+    * v3 documentation
 
-    * v3 documentation - https://developers.virustotal.com/v3.0/reference#ip-addresses
+    https://developers.virustotal.com/reference/ip-info
+    https://developers.virustotal.com/reference/ip-object#relationships
+    https://developers.virustotal.com/reference/ip-votes-post
+
+    * v2 documentation - https://developers.virustotal.com/v2.0/reference/ip-address-report
 """
 from virustotal_python import Virustotal
 from pprint import pprint
 
-API_KEY = "Insert API key here."
+API_KEY = "<VirusTotal API Key>"
 
-# Example IP address (Google DNS)
+# (Google DNS)
 IP = "8.8.8.8"
 
-# v2 examples
+# v3 examples
 vtotal = Virustotal(API_KEY=API_KEY)
 
-## Retrieve information about an IP address
-resp = vtotal.request("ip-address/report", params={"ip": IP})
-
-pprint(resp.json())
-
-# v3 examples
-vtotal = Virustotal(API_KEY=API_KEY, API_VERSION="v3")
-
-# Retrieve information about an IP address
+# Get information about an IP address
 resp = vtotal.request(f"ip_addresses/{IP}")
-# Retrieve objects (relationships) related to an IP address
-# Retrieve historical_whois relationship to the IP address
-# For other relationships, see the table at: https://developers.virustotal.com/v3.0/reference#ip-relationships
+# Get objects (relationships) related to an IP address
+# Get historical_whois relationship to the IP address
 resp = vtotal.request(f"ip_addresses/{IP}/historical_whois")
-# Retrieve communicating_files related to the IP address with a limit of 5
-resp = vtotal.request(
-    f"ip_addresses/{IP}/communicating_files", params={"limit": 5})
+# Get communicating_files related to the IP address with a limit of 5
+resp = vtotal.request(f"ip_addresses/{IP}/communicating_files", params={"limit": 5})
 
 # Retrieve votes for an IP address
 resp = vtotal.request(f"ip_addresses/{IP}/votes")
-# Send a vote for an IP address
-# Create vote JSON
-## Verdict can be either harmless or malicious
-### https://developers.virustotal.com/v3.0/reference#ip-votes-post
+# Add a vote for an IP address
+# Verdict can be either harmless or malicious
 vote = {"data": {"type": "vote", "attributes": {"verdict": "harmless"}}}
 resp = vtotal.request(f"ip_addresses/{IP}/votes", json=vote, method="POST")
+
+# v2 examples
+vtotal = Virustotal(API_KEY=API_KEY, API_VERSION=2)
+# Get information about an IP address
+resp = vtotal.request("ip-address/report", params={"ip": IP})
+pprint(resp.json())
